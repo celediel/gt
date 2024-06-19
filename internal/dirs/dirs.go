5942eee85da3161865fa004d0c6337088151f4c2
Lilian Jónsdóttir <lilian.jonsdottir@gmail.com>
2024-06-19 16:35:40 -0700

    replace $PWD in unexpand

--- internal/dirs/dirs.go
+++ internal/dirs/dirs.go
@@ -12,10 +12,18 @@ import (
 func UnExpand(dir string) (outdir string) {
 	var (
 		home = os.Getenv("HOME")
+		pwd  string
+		err  error
 	)
 
 	outdir = filepath.Clean(dir)
-	outdir = strings.ReplaceAll(outdir, home, "~")
+
+	pwd, err = os.Getwd()
+	if err == nil {
+		outdir = strings.Replace(outdir, pwd, ".", 1)
+	}
+
+	outdir = strings.Replace(outdir, home, "~", 1)
 
 	return
 }
